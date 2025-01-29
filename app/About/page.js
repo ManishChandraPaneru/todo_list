1dc0eaf90aa91f73b0bbcf7f8233a87c88133fb5
Manish Chandra Paneru <manishpaneru2010@gmail.com>
2025-01-29 20:40:07 +0530

    Trying to create a todo list application to practice React.js+Next.js.

---
+++ app/About/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Hello Baby....</div>
+  )
+}
+
+export default page
--- app/page.js
+++ app/page.js
@@ -1,101 +1,31 @@
-import Image from "next/image";
+"use client"
+import React, { useState } from 'react'
 
-export default function Home() {
+const page = () => {
+  const [title, settitle] = useState("");
+  const [desc, setdesc] = useState("") 
+  const submitHander=(e)=>{
+    e.preventDefault()
+    console.log(title)
+    console.log(desc)
+    setdesc("")
+    settitle("")
+  }
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    <>
+      <div className=" flex flex-col items-center content-center">
+        <h1 className='bg-black text-white p-5 text-2xl font-bold text-center w-full'>Manish's TodoList</h1>
+        <form onSubmit={submitHander}>
+          <input type="text" value={title} onChange={(e)=>{settitle(e.target.value)}} className='text-2xl border-zinc-800 border-2 m-5 px-4 py-2' placeholder='Enter task here' />
+          <input type="text" value={desc} onChange={(e)=>{setdesc(e.target.value)}} name="" id="" className='text-2xl border-zinc-800 border-2 m-5 px-4 py-2' placeholder='Enter description here' />
+          <button className='bg-black text-white px-4 py-2 text-2xl font-bold rounded'>Add Task</button>
+        </form>
+      </div>
+    </>
+  )
 }
+
+export default page
+
+//My definition-Two Way Binding: As we can see that we are making changes in variables like title and desc which are being redirected to input tag. So we can say that it binds react variables to input tag or frontend to the user and vice versa which can be seen as binding two things so it is known as two way binding.
+//Co-pilot's definition-In React, two-way binding refers to the process where changes in the UI (input fields) are reflected in the state variables, and changes in the state variables are reflected in the UI. This is achieved using the value attribute and the onChange event handler.
